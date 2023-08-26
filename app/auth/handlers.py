32b8ace42450a0fe28362b927b9ce6a769f9ea8d
IlyaBulatau <ilyabulatau@gmail.com>
2023-08-26 08:09:08 +0200

    update user models and auth handlers

--- app/auth/handlers.py
+++ app/auth/handlers.py
@@ -1,15 +1,23 @@
 from fastapi import APIRouter
 from fastapi.security import OAuth2PasswordBearer
 
+from app.schemas.user import UserAuth
+
 
 router = APIRouter(prefix="/auth", tags=["auth"])
 oauth_schema = OAuth2PasswordBearer("signin", scheme_name="get_token", description="get token from headers")
 
 
-@router.post(path="/signup", response_model=...)
-async def signup_process():
+@router.post(path="/signup", response_model=..., status_code=201)
+async def signup_process(user: UserAuth):
+    """
+    Handles registration process
+    """
     ...
 
 @router.post(path="/signin")
 async def signin_process():
+    """
+    Handles getting token process
+    """
     ...
--- app/schemas/user.py
+++ app/schemas/user.py
@@ -1,20 +1,19 @@
-from pydantic import BaseModel, EmailStr, Field, validator
+from pydantic import BaseModel, EmailStr, Field, validator, UUID4
 
 import re
+import uuid
 
 
-class UserAuth(BaseModel):
+class UserBase(BaseModel):
+    """
+    Base user model
+    """
     username: str = Field(alias="login", 
                           title="Username", 
                           description="Username for registration process",
                           min_length=4,
                           max_length=20)
     email: EmailStr
-    password: str = Field(alias="password", 
-                          title="Password",
-                          description="User password",
-                          min_length=8,
-                          max_length=90)
     
     @validator("username")
     def username_validate(cls, username: str) -> str:
@@ -23,9 +22,22 @@ class UserAuth(BaseModel):
             raise ValueError("Username must be a word or set of words separated by 1 space or underscore")
         return username
     
+
+class UserAuth(UserBase):
+    password: str = Field(alias="password", 
+                          title="Password",
+                          description="User password",
+                          min_length=8,
+                          max_length=90)
+    
+    
     @validator("password")
     async def password_validate(cls, password: str) -> str:
         result = re.match(r'^.*(?=.{8,})(?=.*[a-zA-Z])(?=.*\d)(?=.*[!#$%&? "]).*$', password)
-        if not password:
+        if not result:
             raise ValueError("the Password must be more than 8 characters long and contain letters of the English alphabet numbers and special characters")
         return password
+
+
+class UserResponce(UserBase):
+    id: UUID4 = Field(default_factory=uuid.uuid4)
