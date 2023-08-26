1d1b80404966208d46a7e7da3d1b4db9d926e852
IlyaBulatau <ilyabulatau@gmail.com>
2023-08-26 09:37:59 +0200

    setup alembic

--- migrations/env.py
+++ migrations/env.py
@@ -5,26 +5,26 @@ from sqlalchemy import pool
 
 from alembic import context
 
-# this is the Alembic Config object, which provides
-# access to the values within the .ini file in use.
+from app.settings.config import DataBaseSettings
+from app.database.models.user import Base
+
+
+settings = DataBaseSettings()
 config = context.config
 
-# Interpret the config file for Python logging.
-# This line sets up loggers basically.
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
-# add your model's MetaData object here
-# for 'autogenerate' support
-# from myapp import mymodel
-# target_metadata = mymodel.Base.metadata
-target_metadata = None
+section = config.config_ini_section
 
-# other values from the config, defined by the needs of env.py,
-# can be acquired:
-# my_important_option = config.get_main_option("my_important_option")
-# ... etc.
+config.set_section_option(section, "DB_LOGIN", settings.DB_LOGIN)
+config.set_section_option(section, "DB_PASSWORD", settings.DB_PASSWORD)
+config.set_section_option(section, "DB_HOST", settings.DB_HOST)
+config.set_section_option(section, "DB_PORT", settings.DB_PORT)
+config.set_section_option(section, "DB_NAME", settings.DB_NAME)
 
+
+target_metadata = Base.metadata
 
 def run_migrations_offline() -> None:
     """Run migrations in 'offline' mode.
