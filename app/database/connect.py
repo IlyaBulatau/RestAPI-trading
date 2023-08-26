e96397ac1a42f6d1152ac9f160ee92dfe06d14b0
IlyaBulatau <ilyabulatau@gmail.com>
2023-08-26 09:23:57 +0200

    create engine session

---
+++ app/database/connect.py
@@ -0,0 +1,19 @@
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
+
+
+from app.settings.config import DataBaseSettings
+
+
+async_engine = create_async_engine(url=DataBaseSettings().get_url(), echo=True, future=True)
+async_session = async_sessionmaker(bind=async_engine, expire_on_commit=False, class_=AsyncSession) 
+
+
+async def get_session():
+    session = async_session()
+    try:
+        yield session
+        await session.commit()
+    except:
+        await session.rollback()
+    finally:
+        await session.close()
