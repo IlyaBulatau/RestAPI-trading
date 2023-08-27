96a65d5a8f6a5bb9026af23c077cea6af3483ad5
IlyaBulatau <ilyabulatau@gmail.com>
2023-08-27 07:44:04 +0200

    add token schema and refactor signin handler

--- app/auth/actions.py
+++ app/auth/actions.py
@@ -4,15 +4,19 @@ from app.utils.helpers import verify_password
 from app.database.models.user import User
 from app.database.db import Database
 
+from fastapi.exceptions import HTTPException
+from fastapi import status
+
+
 async def authenticate_user(user: UserLogin) -> User:
     """
     The func conducts the authentication process
     """
     user_from_db = await Database().get_user_by_email(user.email)
     if not user_from_db:
-        return False
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="You email not found")
     if not verify_password(password=user.password, hash_password=user_from_db.hash_password):
-        return False
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Password is invalid")
     return user_from_db
     
 async def is_exists_user(user: UserAuth) -> bool:
--- app/auth/handlers.py
+++ app/auth/handlers.py
@@ -1,9 +1,13 @@
-from fastapi import APIRouter, status
+from fastapi import APIRouter, status, Depends
 from fastapi.responses import JSONResponse
 from fastapi.security import OAuth2PasswordBearer
 
+from typing import Annotated
+
 from app.schemas.user import UserAuth, UserResponce, UserLogin
+from app.schemas.token import Token
 from app.database import db
+from app.database.models.user import User
 from app.auth.actions import authenticate_user, is_exists_user
 from app.utils.helpers import generate_token
 
@@ -21,23 +25,21 @@ async def signup_process(user: UserAuth):
     Handles registration process
     """
     if is_exists_user(user):
-        return JSONResponse(content={"Result": {"Status": 200, "User status": "Is exists"}})
+        return JSONResponse(content={"Result": {"Status": 200, "Operation": "Not exists, this is user is exists"}})
     user_id = await db.Database().create_user_in_db(user)
     return UserResponce(username=user.username, email=user.email, id=user_id)
 
 @router.post(path="/signin",  
             status_code=200,
+            response_model=Token,
             response_description="Responce token")
-async def signin_process(user: UserLogin):
+async def signin_process(user: Annotated[User, Depends(authenticate_user)]):
     """
     Handles getting token process
     """
-    is_user = await authenticate_user(user)
-    if not is_user:
+    if not user:
         return JSONResponse(content={"Error": {"Invalid Data": "Check correct username and password", "Status": 401}}, 
                        status_code=status.HTTP_401_UNAUTHORIZED)
     
-    token = generate_token(payload={"user_id": str(is_user.id), "email":is_user.email})
-    return JSONResponse(content={"OK": {"token": token}}, 
-                    status_code=status.HTTP_200_OK)
-    
+    token = generate_token(payload={"user_id": str(user.id), "email":user.email})
+    return Token(token=token)
