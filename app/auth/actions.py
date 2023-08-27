1a973c8efceed585f33dbe4aff8a80b331d4c374
IlyaBulatau <ilyabulatau@gmail.com>
2023-08-27 09:12:05 +0200

    refactoring token validations

--- app/auth/actions.py
+++ app/auth/actions.py
@@ -4,9 +4,8 @@ from app.utils.helpers import verify_password
 from app.database.models.user import User
 from app.database.db import Database
 from app.settings.config import TokenSettings
-from app.schemas.token import Payload
+from app.auth.verify_token import verify_payload_from_token, verify_token_email, verify_token_time
 
-from datetime import datetime
 from typing import Annotated
 
 from jose import jwt
@@ -16,6 +15,7 @@ from fastapi.security import OAuth2PasswordBearer
 
 
 oauth_schema = OAuth2PasswordBearer("signin", scheme_name="get_token", description="get token from authorization headers")
+
 
 async def authenticate_user(user: UserLogin) -> User:
     """
@@ -43,41 +43,6 @@ async def get_token_payload(token: Annotated[str, Depends(oauth_schema)]):
     """
     payload = jwt.decode(token=token, key=TokenSettings().TOKEN_KEY, algorithms=TokenSettings().TOKEN_ALGORITHM)
     return payload
-
-async def verify_token_time(exp: datetime) -> bool:
-    """
-    Checking token time exp and compare with datetime now
-    """
-    if datetime.utcnow() > exp:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Token time is up")
-
-async def verify_token_email(email: str) -> User:
-    """
-    Check if there is an email in the database
-    return user
-    """
-    user = await Database().get_user_by_email(email)
-    if user:
-        return user
-    raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="You token is invalid")
-
-
-async def verify_payload_from_token(payload: dict) -> Payload:
-    """
-    Check if payload compares to schema
-    return Payload schema
-    """
-    # copy data
-    copy_payload = payload.copy()
-    # covert second to datetime
-    copy_payload["exp"] = datetime.fromtimestamp(copy_payload["exp"])
-    # create Payload object
-    verify_payload = Payload(**copy_payload)
-
-    if copy_payload != dict(verify_payload):
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid token")
-    return verify_payload
-
 
 async def get_current_user(payload: Annotated[dict, Depends(get_token_payload)]) -> User:
     """
---
+++ app/auth/verify_token.py
@@ -0,0 +1,43 @@
+from datetime import datetime
+
+from app.schemas.token import Payload
+from app.database.models.user import User
+from app.database.db import Database
+
+from fastapi import status
+from fastapi.exceptions import HTTPException
+
+
+async def verify_token_time(exp: datetime) -> None:
+    """
+    Checking token time exp and compare with datetime now
+    """
+    if datetime.utcnow() > exp:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Token time is up")
+
+async def verify_token_email(email: str) -> User:
+    """
+    Check if there is an email in the database
+    return user
+    """
+    user = await Database().get_user_by_email(email)
+    if user:
+        return user
+    raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="You token is invalid")
+
+
+async def verify_payload_from_token(payload: dict) -> Payload:
+    """
+    Check if payload compares to schema
+    return Payload schema
+    """
+    # copy data
+    copy_payload = payload.copy()
+    # covert second to datetime
+    copy_payload["exp"] = datetime.fromtimestamp(copy_payload["exp"])
+    # create Payload object
+    verify_payload = Payload(**copy_payload)
+
+    if copy_payload != dict(verify_payload):
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid token")
+    return verify_payload
