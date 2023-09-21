5c47ed6f9cb85d4b0fad662cf5a64bad8399bfe5
IlyaBulatau <ilyabulatau@gmail.com>
2023-09-21 19:27:15 +0200

    mod log.py

--- app/utils/log.py
+++ app/utils/log.py
@@ -5,7 +5,7 @@ from logging.handlers import SMTPHandler
 from app.settings.config import SMTPSettings
 
 
-LOG_FILE = Path().absolute().joinpath("app").joinpath("utils")
+LOG_FILE = Path().absolute().joinpath("app").joinpath("utils").joinpath("log.log")
 
 formatter = logging.Formatter(style="{", fmt="{asctime} | {levelname} | {message}")
 
@@ -20,14 +20,19 @@ file_handler = logging.FileHandler(filename=LOG_FILE, encoding="utf-8")
 file_handler.setFormatter(formatter)
 file_handler.setLevel("DEBUG")
 
-if SMTPSettings.LOGGER_EMAIL and SMTPSettings.LOGGER_PASSWORD:
+if SMTPSettings().LOGGER_EMAIL and SMTPSettings().LOGGER_PASSWORD:
     smtp_handler = SMTPHandler(
-        mailhost=(SMTPSettings.LOGGER_HOST, SMTPSettings.LOGGER_PORT),
-        fromaddr=SMTPSettings.LOGGER_EMAIL,
-        toaddrs=SMTPSettings.LOGGER_EMAIL,
-        subject=SMTPSettings.LOGGER_SUBJECT,
-        credentials=(SMTPSettings.LOGGER_EMAIL, SMTPSettings.LOGGER_PASSWORD),
+        mailhost=(SMTPSettings().LOGGER_HOST, SMTPSettings().LOGGER_PORT),
+        fromaddr=SMTPSettings().LOGGER_EMAIL,
+        toaddrs=SMTPSettings().LOGGER_EMAIL,
+        subject=SMTPSettings().LOGGER_SUBJECT,
+        credentials=(SMTPSettings().LOGGER_EMAIL, SMTPSettings().LOGGER_PASSWORD),
         secure=(),
     )
     smtp_handler.setFormatter(formatter)
     smtp_handler.setLevel("CRITICAL")
+
+    logger.addHandler(smtp_handler)
+
+logger.addHandler(stream_handler)
+logger.addHandler(file_handler)
