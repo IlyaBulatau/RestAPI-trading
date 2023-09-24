9c2b40fe9191c0c5d08f4e31783866f337596a35
IlyaBulatau <ilyabulatau@gmail.com>
2023-09-24 08:18:00 +0200

    create tests skeleton

---
+++ tests/api/test_endpoints.py
@@ -0,0 +1,10 @@
+class TestUserAuthHandler:
+    ...
+
+
+class TestUserHandler:
+    ...
+
+
+class TestProductHandler:
+    ...
---
+++ tests/db/test_crud.py
@@ -0,0 +1,6 @@
+class TestUserCRUD:
+    ...
+
+
+class TestProductCRUD:
+    ...
